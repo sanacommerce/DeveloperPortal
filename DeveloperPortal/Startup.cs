d04fa18ed8e0c89e344ee4bd1b0cbdc7a37d74fe
Lucas <lucas.jongsma@live.nl>
2021-09-20 13:46:33 +0200

    Iniatiated api update

--- DeveloperPortal/Startup.cs
+++ DeveloperPortal/Startup.cs
@@ -5,13 +5,41 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DeveloperPortal
 {
     public class Startup
     {
+        private static string git_api = @"https://github.com/login/oauth/access_token";
+
+        public string Post(string uri, string data, string contentType, string method = "POST")
+        {
+            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.ContentLength = dataBytes.Length;
+            request.ContentType = contentType;
+            request.Method = method;
+
+            using(Stream requestBody = request.GetRequestStream())
+            {
+                requestBody.Write(dataBytes, 0, dataBytes.Length);
+            }
+
+            using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using(Stream stream = response.GetResponseStream())
+            using(StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
@@ -23,6 +51,14 @@ namespace DeveloperPortal
                 app.UseDeveloperExceptionPage();
             }
             app.UseRouting();
+            app.Use( async (context, next) => {
+                string code = context.Request.Query["code"].ToString();
+                if (!string.IsNullOrEmpty(code)) {
+                    string res = Post(git_api,@"" , "application/json");
+                }
+                await next();
+            }
+            );
             app.UseDefaultFiles();
             app.UseStaticFiles();
         }
